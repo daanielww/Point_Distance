10dad2a92ed3294e1a524cb81975264608c67d22
Daniel Wang <daniel.wang.1999@hotmail.com>
2019-02-11 17:16:53 -0500

    completed

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import cv2
 import numpy as np
 
+coord = []
+
+def onMouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+       global coord
+       coord.append(np.array((x ,y)))
+
+
 
 def loadImage():
     img = cv2.imread("C:\\Users\\danie\\Desktop\\rectified_images\\undistorted0.jpg")
-    print("22")
     cv2.imshow('Image', img)
     pressedkey = cv2.waitKey(0)
 
     # Wait for ESC key to exit
     if pressedkey == 27:
         cv2.destroyAllWindows()
+        global coord
+        dist = np.linalg.norm(coord[0]-coord[1])
+        print(dist)
 
+
+cv2.namedWindow('Image')
+cv2.setMouseCallback('Image', onMouse)
 loadImage()
+
